4a925fdfc6234a4c015b6ae0f6f7abb8718f7240
tyler broadway <tbroadway96@gmail.com>
2020-08-17 16:14:47 -0500

    Completed Linear Search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,9 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
-
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
